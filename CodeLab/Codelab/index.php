26ab9a020c20069f0940669ad74f5208ab26e605
Rahmatullah Akbar Prima <117903731+Arabmeme@users.noreply.github.com>
2024-11-01 14:50:59 +0700

    Delete CodeLab/Codelab directory

--- CodeLab/Codelab/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include "Controllers/ProductController.php";
-
-use Controllers\ProductController;
-
-$productController = new ProductController();
-echo $productController->getALLProduct();  // Sesuaikan dengan nama method di class
